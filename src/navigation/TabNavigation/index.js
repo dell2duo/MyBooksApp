5fe9082ea3b38b3e0d7bb01e5c1a86d2ea98309d
dell2duo <rafael.rocha.ufv@gmail.com>
2021-06-05 23:29:46 -0300

    add styled tabbar

--- src/navigation/TabNavigation/index.js
+++ src/navigation/TabNavigation/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import uuid from "react-native-uuid";
 
+import { Home as HomeIcon, Book, User } from "react-native-feather";
+
 import Home from "../../pages/Home";
+import { TouchableOpacity } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,23 +15,64 @@ const stackRoutes = [
     component: Home,
   },
   {
-    name: "libraries",
+    name: "Libraries",
     component: Home,
   },
-  // {
-  //   name: "profile",
-  //   component: TabNavigation,
-  // },
+  {
+    name: "Profile",
+    component: Home,
+  },
 ];
+
+const tabBarOptions = {
+  activeTintColor: "#000",
+  inactiveTintColor: "#BFBEBF",
+  labelStyle: {
+    paddingTop: 10,
+    fontSize: 10,
+    fontFamily: "sfpro-display-medium",
+  },
+  style: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    height: 65,
+    elevation: 0,
+  },
+  keyboardHidesTabBar: true,
+};
 
 const TabNavigation = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      sceneContainerStyle={{ backgroundColor: "transparent" }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          console.log(size);
+          switch (route.name) {
+            case "Home":
+              return <HomeIcon stroke={color} height={size} width={size} />;
+            case "Libraries":
+              return <Book stroke={color} height={size} width={size} />;
+            case "Profile":
+              return <User stroke={color} height={size} width={size} />;
+
+            default:
+              break;
+          }
+        },
+      })}
+      tabBarOptions={tabBarOptions}
+    >
       {stackRoutes.map((route) => (
         <Tab.Screen
           key={uuid.v4()}
           name={route.name}
           component={route.component}
+          options={{
+            tabBarButton: (props) => (
+              <TouchableOpacity activeOpacity={0.7} {...props} />
+            ),
+          }}
         />
       ))}
     </Tab.Navigator>
